72eb069820befab6a9a617dfc045bf453ab31810
Urobosoft <quantum.rumblers079@gmail.com>
2024-04-14 16:58:06 -0600

    Políticas

--- client/src/components/Login/RegisterForm.jsx
+++ client/src/components/Login/RegisterForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../../context/authContext";
+import "../../styles/popup.css"
 
 const RegisterForm = () => {
   const [formData, setFormData] = useState({
@@ -98,12 +99,36 @@ const RegisterForm = () => {
   const PoliciesPopup = ({ onClose }) => (
     <div className="popup-background">
       <div className="popup-container">
-        <h2>Policies</h2>
+        <h2>Políticas de Privacidad</h2>
         <p>
-          Aquí puedes incluir todos los términos, condiciones y políticas de
-          privacidad de tu servicio. Asegúrate de proporcionar información clara
-          y precisa a tus usuarios.
-        </p>
+        Bienvenido a VitalIA. Nuestra misión es mejorar la gestión y la atención en el sector salud mediante herramientas digitales avanzadas. Este documento detalla cómo recopilamos, utilizamos y protegemos la información personal en nuestras aplicaciones web y móviles.<br /><br />
+        Información que recopilamos<br />
+        Datos de registro: Incluyen ID de usuario, nombre, fecha de nacimiento, género, correo electrónico y contraseña.<br />
+        Información médica: Historial clínico, enfermedades crónicas, diagnósticos, tratamientos y otros detalles médicos pertinentes.<br />
+        Datos de uso: Información sobre cómo interactúa con nuestra aplicación, incluyendo fechas y horas de acceso.<br /><br />
+        Uso de la información<br />
+        Utilizamos la información recopilada para:<br />
+        - Facilitar la gestión hospitalaria y la automatización de registros médicos.<br />
+        - Apoyar diagnósticos y tratamientos mediante inteligencia artificial.<br />
+        - Mejorar y personalizar la experiencia del usuario.<br />
+        - Cumplir con obligaciones legales y regulaciones aplicables.<br /><br />
+        Compartir información con terceros<br />
+        No vendemos ni alquilamos información personal a terceros. La información puede ser compartida solo en los siguientes casos:<br />
+        - Con proveedores de servicios que nos ayudan a operar la aplicación, siempre bajo acuerdos que protegen su privacidad.<br />
+        - Por requerimientos legales o para proteger derechos y seguridad.<br /><br />
+        Seguridad de la información<br />
+        Aplicamos medidas de seguridad técnicas y organizativas para proteger su información, incluyendo encriptación de datos y sistemas de autenticación segura. Aunque tomamos precauciones significativas, ninguna medida de seguridad es completamente infalible.<br /><br />
+        Derechos del usuario<br />
+        Los usuarios pueden acceder, corregir o eliminar su información personal. Además, tienen derecho a limitar el uso y divulgación de sus datos. Para ejercer estos derechos, pueden contactarnos a través de los detalles proporcionados al final de este documento.<br /><br />
+        Uso de cookies y tecnologías similares<br />
+        Utilizamos cookies para mejorar la funcionalidad de nuestras aplicaciones. Los usuarios pueden configurar su navegador para rechazar cookies si lo prefieren.<br /><br />
+        Enlaces a otros sitios<br />
+        Nuestra aplicación puede contener enlaces a sitios web externos que no operamos. No somos responsables de las prácticas de privacidad de estos sitios.<br /><br />
+        Cambios a la política de privacidad<br />
+        Podemos actualizar nuestras políticas de privacidad ocasionalmente. Los cambios serán notificados a través de nuestra aplicación o por correo electrónico.<br /><br />
+        Contacto<br />
+        Para preguntas o preocupaciones sobre nuestra política de privacidad, por favor contacte a nuestro equipo de soporte a través de grupo.operativo.84@gmail.com.
+      </p>
         <button onClick={onClose}>Cerrar</button>
       </div>
     </div>
