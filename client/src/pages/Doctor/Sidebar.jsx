6bea4ccbf434345a3ecd6234e3545a319c6d5442
Urobosoft <quantum.rumblers079@gmail.com>
2024-05-22 21:11:41 -0600

    Configure backend to serve frontend and add heroku-postbuild script

--- client/src/pages/Doctor/Sidebar.jsx
+++ client/src/pages/Doctor/Sidebar.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import logo from "./Godev.svg";
+// // import logo from "./Godev.svg";
 import { v } from "../../styles/Variables";
 import {
   AiOutlineLeft,
@@ -35,7 +35,7 @@ export function Sidebar({ sidebarOpen, setSidebarOpen }) {
       </button>
       <div className="Logocontent">
         <div className="imgcontent">
-          <img src={logo} />
+  
         </div>
         <h2>VitalIA</h2>
       </div>
