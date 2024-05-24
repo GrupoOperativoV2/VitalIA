908cb9272c678625430373847aec81e028fa3c26
Urobosoft <quantum.rumblers079@gmail.com>
2024-05-24 11:18:37 -0600

    Add JWT authentication

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://grupooperativo84:QVsgqSH75aHJyNmi@vitalia.qaqiuht.mongodb.net/?retryWrites=true&w=majority";
 //export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
 
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+export const JWT_SECRET = process.env.JWT_SECRET || "secret";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "https://vital-ia-c973e2aac327.herokuapp.com";
