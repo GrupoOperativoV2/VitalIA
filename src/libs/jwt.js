7ad25406655c96f2dc200a688eb0149adb12001f
Urobosoft <quantum.rumblers079@gmail.com>
2024-05-24 11:15:14 -0600

    Add JWT authentication

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,9 +1,9 @@
-import { TOKEN_SECRET } from "../config.js";
+import { JWT_SECRET } from "../config.js";
 import jwt from "jsonwebtoken";
 
 export async function createAccessToken(payload) {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
+    jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" }, (err, token) => {
       if (err) reject(err);
       resolve(token);
     });
